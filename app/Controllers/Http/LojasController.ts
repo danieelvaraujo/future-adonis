f1c6ecc29de987ecd377d4fa7eba68ff4bf1d92b
Daniel Valente <danieelvaraujo@hotmail.com>
2023-01-17 22:11:38 -0300

    chore: reworked lojas controller

--- app/Controllers/Http/LojasController.ts
+++ app/Controllers/Http/LojasController.ts
@@ -1,25 +1,47 @@
-import Database from '@ioc:Adonis/Lucid/Database'
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
+import Loja from 'App/Models/Loja';
 export default class LojasController {
-    public async index() {
-        return Database.from('lojas').select('*')
+    public async index({ response }) {
+        const lojas = await Loja.all()
+
+        return response.ok(lojas)
     }
 
-    public async store(ctx: HttpContextContract) {
-        const requestBody = ctx.request.body();
+    public async show({ params, response }) {
+        const { id }: { id: Number } = params
 
-        await Database
-            .table('lojas')
-            .insert({
-                titulo: requestBody.titulo,
-                documento: requestBody.documento,
-                tipo: requestBody.tipo,
-                horario_abertura: requestBody.horario_abertura,
-                horario_fechamento: requestBody.horario_fechamento
-            })
-            .returning('id') // For PostgreSQL
+        const loja: any = await Loja.find(id)
+        if (!loja) {
+            return response.notFound({ message: 'Loja não encontrada.' })
+        }
 
-        return "Loja adicionada com sucesso."
+        return response.ok(loja)
+    }
+
+    public async store({ request, response }) {
+        const storeSchema = schema.create({
+            titulo: schema.string({ trim: true }, [
+                rules.required(),
+                rules.maxLength(100)
+            ]),
+            documento: schema.string({ escape: true }, [
+                rules.maxLength(14)
+            ]),
+            tipo: schema.string({ escape: true }, [
+                rules.required(),
+            ]),
+            horario_abertura: schema.string({ escape: true }, [
+                rules.required(),
+            ]),
+            horario_fechamento: schema.string({ escape: true }, [
+                rules.required(),
+            ]),
+        })
+
+        const payload: any = await request.validate({ schema: storeSchema })
+        const loja: Loja = await Loja.create(payload)
+
+        return response.ok(loja)
     }
 }
