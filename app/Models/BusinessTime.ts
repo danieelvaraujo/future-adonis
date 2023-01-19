cff7838c49db5ed2c4405ee8c22b45d895555ba9
Daniel Valente <danieelvaraujo@hotmail.com>
2023-01-18 23:01:44 -0300

    chore: added business time model

---
+++ app/Models/BusinessTime.ts
@@ -0,0 +1,32 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import Days from 'Contracts/Enums/Days'
+import Store from './Store'
+
+export default class BusinessTime extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public storeId: number
+
+  @column()
+  public day: Days
+
+  @column()
+  public openingHour?: string
+
+  @column()
+  public closingHour?: string  
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @hasOne(() => Store, {
+    foreignKey: 'storeId'
+  })
+  public store: HasOne<typeof Store>
+}
