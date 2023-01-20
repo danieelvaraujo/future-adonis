821d5574f1402a936fc0cd0ec1e9492778da2398
Daniel Valente <danieelvaraujo@hotmail.com>
2023-01-20 10:19:11 -0300

    test: update method testcase slightly changed

--- tests/functional/store/update.spec.ts
+++ tests/functional/store/update.spec.ts
@@ -5,13 +5,13 @@ test.group('Store update', () => {
   test('get a success message when updating store with optional values', async ({ client }) => {
     const store = await StoreFactory.create()
     const body = {
-      title: "Tenda do Joaquim",
+      document: "62290108000113"
     }
 
-    const response = await client.put(`/stores/${store.id}`)
+    const response = await client.put('/stores/' + store.id)      
       .form(body)
-    console.log(response.error())
+          
     response.assertStatus(200)
-    response.assertBodyContains({ title: body.title })
+    response.assertBodyContains({ document: body.document })
   })
 })
