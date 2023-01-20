3c37fbff350c2694ca8f5066b3fe1200e84fbda8
Daniel Valente <danieelvaraujo@hotmail.com>
2023-01-20 10:17:29 -0300

    chore: changed error string

--- app/Controllers/Http/Helpers/Messages.ts
+++ app/Controllers/Http/Helpers/Messages.ts
@@ -2,7 +2,7 @@ export const messages = {
     store: {
         NOT_FOUND: 'A loja não foi encontrada.',
         REMOVED: 'A loja foi removida com sucesso.',
-        ERROR: 'Houve um problema ao tentar remover a loja.',
+        ERROR: 'Houve um problema ao tentar remover a loja: ',
         EMPTY: 'Ainda não há lojas cadastradas.'
     }
 }
