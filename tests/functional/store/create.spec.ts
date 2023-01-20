5a6bfcc15c879aea20ccc06156b21022b8daa6b1
Daniel Valente <danieelvaraujo@hotmail.com>
2023-01-20 09:18:47 -0300

    test: fix create store test

--- tests/functional/store/create.spec.ts
+++ tests/functional/store/create.spec.ts
@@ -3,23 +3,24 @@ import { test } from '@japa/runner'
 test.group('Store create', () => {
   test('get a success message when creating store with correct values', async ({ client }) => {
     const body = {
-      title: "Bard Fodido",
+      title: "Bar do Xexél",
       document: "52266679000169",
       businessType: 'bar',
       businessTimes: [
-          {
-              day: "SEGUNDA",
-              opening_hour: "08:00",
-              closing_hour: "12:00"
-          },{
-            day: "TERCA",
-            opening_hour: "08:00",
-            closing_hour: "16:00"
+        {
+          day: "SEGUNDA",
+          opening_hour: "08:00",
+          closing_hour: "12:00"
         },
         {
-            day: "QUARTA",
-            opening_hour: "14:00",
-            closing_hour: "22:00"
+          day: "TERCA",
+          opening_hour: "08:00",
+          closing_hour: "16:00"
+        },
+        {
+          day: "QUARTA",
+          opening_hour: "14:00",
+          closing_hour: "22:00"
         }
       ]
     }
