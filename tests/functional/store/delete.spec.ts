d7f4874eb7d2f292072479bce34da4701a1cc3ee
Daniel Valente <danieelvaraujo@hotmail.com>
2023-01-19 23:12:02 -0300

    test: added delete testcases for store

---
+++ tests/functional/store/delete.spec.ts
@@ -0,0 +1,18 @@
+import { test } from '@japa/runner'
+import { messages } from 'App/Controllers/Http/Helpers/Messages'
+import StoreFactory from 'Database/factories/StoreFactory'
+
+test.group('Store delete', () => {
+  test('get an error message when deleting a non existing store', async ({ client }) => {
+    const response = await client.delete('/stores/51')
+
+    response.assertBodyContains({ message: messages.store.NOT_FOUND })
+  })
+
+  test('get a success message when deleting existing store', async ({ client }) => {
+    await StoreFactory.create()
+    const response = await client.delete(`/stores/1`)
+
+    response.assertStatus(200)    
+  })
+})
